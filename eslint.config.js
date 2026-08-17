2b0045ed03a6c4d4558bf252811ce689fddf9864
Parsa MK <p.mohtashami.k@gmail.com>
2026-08-16 22:51:13 -0700

    init prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import prettier from 'eslint-config-prettier';
 import path from 'node:path';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
@@ -12,6 +13,8 @@ export default defineConfig(
 	js.configs.recommended,
 	ts.configs.recommended,
 	svelte.configs.recommended,
+	prettier,
+	svelte.configs.prettier,
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 		rules: {
