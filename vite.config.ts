95008c7d4bda612a0d7cf424e3b21186b686695f
parsmk <p.mohtashami.k@gmail.com>
2026-08-22 02:08:15 +0000

    init drizzle

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,12 @@ export default defineConfig({
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter(),
+
+			typescript: {
+				config: (config) => {
+					config.include.push('../drizzle.config.ts');
+				},
+			},
 		}),
 	],
 });
